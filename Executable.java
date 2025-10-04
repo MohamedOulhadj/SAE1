847060c23a0873f42ae51fba6383d29a1746527b
MohamedOulhadj <mohamed.oulhadj@etu.univ-orleans.fr>
2025-10-04 22:21:38 +0000

    modification via vscode

--- Executable.java
+++ Executable.java
@@ -4,6 +4,7 @@ public static void main(String[] args){
 
 	System.out.println(" Hello World !") ;
 	System.out.println(" Test Video SAE1") ;
+	System.out.println(" Test de push sur git via vscode ") ;
 
   } 
 
