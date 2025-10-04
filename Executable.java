7e7d61a9bbd23950fcab14bd31cd14cf0cc4314b
MohamedOulhadj <mohamed.oulhadj@etu.univ-orleans.fr>
2025-10-04 22:37:07 +0000

    je modifie le fichier executable pour le push

--- Executable.java
+++ Executable.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 
 	System.out.println(" Hello World !") ;
 	System.out.println(" Test Video SAE1") ;
-	System.out.println(" Test de push sur git via vscode ") ;
+	System.out.println(" Test video ") ;
 
   } 
 
